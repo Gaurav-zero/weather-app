c0b6653e41969550f639a77aeedef9bc3027d4de
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-03-08 16:02:59 +0530

    try to display a loading gif(not working)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import "./styles.css";
 import { getWeatherInfo } from "./getWeatherInfo.js";
 import { displayData } from "./displayData.js";
+import loadingGif from "./spinner.gif";
 
 const f= document.querySelector("form");
 const city= document.querySelector("#city");
+const table= document.querySelector("table");
 
 f.addEventListener("submit", (e)=>{
     e.preventDefault();
+    table.style.backgroundImage= loadingGif;
+
     let weatherData= getWeatherInfo(city.value);
     weatherData.then((data)=>{
+        table.style.backgroundImage="none";
         console.log(data);
         displayData(data);
     })
